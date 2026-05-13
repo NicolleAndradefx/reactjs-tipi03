eff96da41917439a7c36430c7bd9bfa3da0885f1
NICOLLE AILEEN ARAUJO DE ANDRADE <nicolle.aaandrade@senacsp.edu.br>
2026-05-13 12:02:26 -0300

    react aula

---
+++ forms/forms/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import MeuFormulario from './components/MeuFormulario';
+
+const App = () => {
+  return (
+    <div className='App'>
+      <h1>Formulário com React</h1>
+      <MeuFormulario />
+    </div>
+  )
+}
+
+export default App;
---
+++ react-http/react-http/src/App.jsx
@@ -0,0 +1,114 @@
+import {useEffect, useState } from 'react';
+
+//custom hook
+import { useFetch } from './hooks/useFetch';
+
+const url = "http://localhost:3000/products";
+
+import './App.css';
+
+
+function App ()  {
+
+  // Resgatando os dados
+
+  //const [products, setProducts] = useState([]);
+
+  //custom hook
+  const {data: items, httpConfig, loading } = useFetch(url);
+
+  /*
+  useEffect(() => {
+    async function getData() {
+      const res = await fetch(url);
+      const data = await res.json();
+
+      setProducts(data);
+
+    }getData();
+  }, []);
+  */
+
+  // envio de dados
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+
+  const handleSubmit = async (e) =>{
+      e.preventDefault();
+
+      const product ={
+        name,
+        price
+      };
+
+      //refatorando post
+      httpConfig(product, "POST");
+
+
+/*
+    const res = await fetch(url, {
+          method: "POST", 
+          headers:{
+            "Content-Type" : "application/json",
+          },
+          body: JSON.stringify(product),
+      });
+    //carregamento dinâmico
+    const addeProduct = await res.json();
+    setProducts((prevProducts) =>[...prevProducts, addeProduct]);
+    */
+};
+  return (
+    <div className='App'>
+
+      <h1>HTTP em React</h1>
+
+      {/* Resgatando dados*/}
+      {/* 
+      <ul className='read-the-docs'>
+        {products.map((product) =>(
+          <li key={product.id}>
+            {product.name} - R$ {product.price}
+          </li>
+          ))}
+      </ul>
+        */}
+
+        {/* loading */}
+        {loading && <p>Carregando...</p>}
+
+        <ul className='read-the-docs'>
+          {items && 
+          items.map((product) => (
+          <li key={product.id}>
+            {product.name} - R$ {product.price}
+            
+          </li>
+          ))}
+        </ul>
+
+      {/* Enviando dados */}
+      <div className="add-product">
+        <form onSubmit={handleSubmit}>
+          
+          <label>
+            <span>Nome</span>
+            <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+          </label>
+
+          <label>
+            <span>Preço</span>
+            <input type="text" value={price} onChange={(e) => setPrice(e.target.value)} />
+          </label>
+        {/* <input type="submit" value="Enviar" /> */}
+
+        {loading && <input type="submit" disabled value="Aguarde"/>}
+        {!loading && <input type="submit" value="Cria"/>}
+        </form>
+      </div>
+          
+    </div>
+  )
+}
+
+export default App;
