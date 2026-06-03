08a03a1fcb3e2e8af46bf959e0e9e3c56519263a
NICOLLE AILEEN ARAUJO DE ANDRADE <nicolle.aaandrade@senacsp.edu.br>
2026-06-03 12:09:13 -0300

    login

--- react-router/react-router/src/App.jsx
+++ react-router/react-router/src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 import { Outlet } from 'react-router-dom';
 
 //Link de navegação entre páginas
-import Navbar from './components/navbar';
+import Navbar from './components/Navbar';
 
 //Search Form
 import SearchParams from './components/SearchParams';
